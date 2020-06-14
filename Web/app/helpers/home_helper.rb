5b980f4abaf66ec4cad7833688f79d2ea3b1f330
Bourbotte Pierre <Yiazmat@localhost.localdomain>
2020-06-14 18:35:56 +0200

    adding connection to multiple website

--- Web/app/helpers/home_helper.rb
+++ Web/app/helpers/home_helper.rb
@@ -1,2 +1,23 @@
+require 'open-uri'
+
 module HomeHelper
+  def mediapart_news
+    doc = Nokogiri::HTML(open('https://www.mediapart.fr/')).title
+    doc
+  end
+
+  def google_news
+    doc = Nokogiri::HTML(open('https://news.google.com')).title
+    doc
+  end
+
+  def lemonde_news
+    doc = Nokogiri::HTML(open('https://www.lemonde.fr/')).title
+    doc
+  end
+
+  def minutes_news
+    doc = Nokogiri::HTML(open('https://www.20minutes.fr/')).title
+    doc
+  end
 end
