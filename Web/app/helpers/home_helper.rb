ea9a113bb9db683dae65aed83fa00d2e6640d6b4
Bourbotte Pierre <Yiazmat@localhost.localdomain>
2020-06-17 16:18:00 +0200

    getting all information, now just need to display them

--- Web/app/helpers/home_helper.rb
+++ Web/app/helpers/home_helper.rb
@@ -1,23 +1,27 @@
 require 'open-uri'
+require 'news-api'
 
 module HomeHelper
-  def mediapart_news
-    doc = Nokogiri::HTML(open('https://www.mediapart.fr/')).title
-    doc
+  def apple_news
+    #n = News.new("5f7bf5b89bf047e2af71798d8984f30b")
+    #n.get_sources(country: 'us', language: 'us')
+    #n.get_everything(q: "apple",  from: "2020-06-01", sortBy: "popularity")
+    #n.get_top_headlines(sources: "bbc-news")
+    #req = open(n)
+    #response_body = req.read
+    url = 'http://newsapi.org/v2/top-headlines?'\
+      'country=us&'\
+      'apiKey=5f7bf5b89bf047e2af71798d8984f30b'
+    req = open(url)
+    response_body = req.read
+    response_body
   end
 
   def google_news
-    doc = Nokogiri::HTML(open('https://news.google.com')).title
-    doc
-  end
-
-  def lemonde_news
-    doc = Nokogiri::HTML(open('https://www.lemonde.fr/')).title
-    doc
-  end
-
-  def minutes_news
-    doc = Nokogiri::HTML(open('https://www.20minutes.fr/')).title
-    doc
+    n = News.new("5f7bf5b89bf047e2af71798d8984f30b")
+    n.get_sources(country: 'us', language: 'us')
+    n.get_everything(q: "google",  from: "2020-06-01", sortBy: "popularity")
+    n.get_top_headlines(sources: "bbc-news")
+    puts n
   end
 end
---
+++ Web/vendor/bundle/ruby/2.6.0/specifications/news-api-0.2.0.gemspec
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+# stub: news-api 0.2.0 ruby lib
+
+Gem::Specification.new do |s|
+  s.name = "news-api".freeze
+  s.version = "0.2.0"
+
+  s.required_rubygems_version = Gem::Requirement.new(">= 0".freeze) if s.respond_to? :required_rubygems_version=
+  s.require_paths = ["lib".freeze]
+  s.authors = ["Oleg Mikhnovich".freeze]
+  s.date = "2019-08-23"
+  s.description = "News API SDK gem".freeze
+  s.email = "oleg5966346@gmail.com".freeze
+  s.homepage = "http://rubygems.org/gems/".freeze
+  s.licenses = ["MIT".freeze]
+  s.rubygems_version = "3.0.3".freeze
+  s.summary = "News API SDK for Ruby".freeze
+
+  s.installed_by_version = "3.0.3" if s.respond_to? :installed_by_version
+end
