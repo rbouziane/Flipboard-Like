93f6ed90b06f81a001927a9edc58e473190df5eb
Bourbotte Pierre <Yiazmat@localhost.localdomain>
2020-06-20 17:46:35 +0200

    done

--- Web/app/helpers/home_helper.rb
+++ Web/app/helpers/home_helper.rb
@@ -2,26 +2,39 @@ require 'open-uri'
 require 'news-api'
 
 module HomeHelper
-  def apple_news
-    #n = News.new("5f7bf5b89bf047e2af71798d8984f30b")
-    #n.get_sources(country: 'us', language: 'us')
-    #n.get_everything(q: "apple",  from: "2020-06-01", sortBy: "popularity")
-    #n.get_top_headlines(sources: "bbc-news")
-    #req = open(n)
-    #response_body = req.read
+  def news
     url = 'http://newsapi.org/v2/top-headlines?'\
       'country=us&'\
       'apiKey=5f7bf5b89bf047e2af71798d8984f30b'
     req = open(url)
-    response_body = req.read
-    response_body
+    response_body = JSON.parse(req.read)
   end
 
-  def google_news
-    n = News.new("5f7bf5b89bf047e2af71798d8984f30b")
-    n.get_sources(country: 'us', language: 'us')
-    n.get_everything(q: "google",  from: "2020-06-01", sortBy: "popularity")
-    n.get_top_headlines(sources: "bbc-news")
-    puts n
+  def news_apple
+    url = 'http://newsapi.org/v2/everything?'\
+      'q=Apple&'\
+      'from=2020-06-20&'\
+      'sortBy=popularity&'\
+      'apiKey=5f7bf5b89bf047e2af71798d8984f30b'
+    req = open(url)
+    response_body = JSON.parse(req.read)
+  end
+
+  def news_google
+    url = 'http://newsapi.org/v2/everything?'\
+      'q=Google&'\
+      'from=2020-06-20&'\
+      'sortBy=popularity&'\
+      'apiKey=5f7bf5b89bf047e2af71798d8984f30b'
+    req = open(url)
+    response_body = JSON.parse(req.read)
+  end
+
+  def news_bbc
+    url = 'http://newsapi.org/v2/top-headlines?'\
+      'sources=bbc-news&'\
+      'apiKey=5f7bf5b89bf047e2af71798d8984f30b'
+    req = open(url)
+    response_body = JSON.parse(req.read)
   end
 end
